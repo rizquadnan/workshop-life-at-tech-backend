dca43b3088cd6713fe776b55482441fde47516ff
rizquadnan <madnanrizqullah@gmail.com>
2024-01-23 18:09:17 +0300

    add todo for login and register

--- src/v1/controllers/auth.controller.ts
+++ src/v1/controllers/auth.controller.ts
@@ -42,6 +42,8 @@ export const registerUserHandler = async (
   res: Response,
   next: NextFunction
 ) => {
+  // TODO: verification code via email
+  // TODO: register via google
   try {
     const userType = req.params.userType;
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
@@ -82,6 +84,7 @@ export const loginUserHandler = async (
   res: Response,
   next: NextFunction
 ) => {
+  // TODO: login via google
   try {
     const { email, password } = req.body;
     const userType = req.params.userType;
